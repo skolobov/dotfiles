abd97af7cdfa4991ffdef00644f95f0ed69ec4e5
Sergei Kolobov <sergei@kolobov.com>
2017-04-12 01:52:10 +0300

    Fish: rearrange and simplify config

--- fish/config.fish
+++ fish/config.fish
@@ -1,34 +1,30 @@
 # Fish shell configuration file
 
-# OMF theme configuration
+# Use English (United States) locale
+set -gx LANG "en_US.UTF-8"
+set -gx LC_ALL "en_US.UTF-8"
+
+# Prompt theme configuration
 set -g default_user skolobov
 
-# Aliases
+# Useful aliasliases
 alias git=hub
 alias g=git
 alias gs='git status --short'
 alias gd='git diff'
 alias vim=nvim
 
+# Proceed only if running an interactive shell
+status --is-interactive; or exit
+
 # Initialize rbenv if present
-status --is-interactive
-  and test -n (which rbenv)
-  and source (rbenv init - | psub)
+if which -s rbenv
+	source (rbenv init - | psub)
+end
 
 # Initialize pyenv if present
-status --is-interactive
-  and test -n (which pyenv)
-  and source (pyenv init - | psub)
-  and source (pyenv virtualenv-init - | psub)
-set -x VIRTUAL_ENV_DISABLE_PROMPT 1
-
-# Locale configuration
-# Prefer English for language and messages
-set -gx LANG "en_US.UTF-8"
-set -gx LC_MESSAGES "en_US.UTF-8"
-# Russian for everything else
-set -gx LC_CTYPE "ru_RU.UTF-8"
-set -gx LC_COLLATE "ru_RU.UTF-8"
-set -gx LC_MONETARY "ru_RU.UTF-8"
-set -gx LC_NUMERIC "ru_RU.UTF-8"
-set -gx LC_TIME "ru_RU.UTF-8"
+if which -s pyenv
+	source (pyenv init - | psub)
+	source (pyenv virtualenv-init - | psub)
+	set -x VIRTUAL_ENV_DISABLE_PROMPT 1
+end
